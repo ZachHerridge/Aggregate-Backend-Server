e3b9492cfd43b963b91e846ed080387bdcaea4a0
Ty Cran <tyler.j.cran@gmail.com>
2019-02-03 02:57:35 -0600

    Fix mix up in Profile and User

--- src/main/java/com/mhack/aggregate/server/api/user/domain/User.java
+++ src/main/java/com/mhack/aggregate/server/api/user/domain/User.java
@@ -1,33 +1,22 @@
 package com.mhack.aggregate.server.api.user.domain;
 
-import com.mhack.aggregate.server.api.user.extra_variables.FarmEquipment;
-import com.mhack.aggregate.server.api.user.extra_variables.FarmType;
-import com.mhack.aggregate.server.api.user.extra_variables.ProfileLocation;
-
-import java.util.HashSet;
+import com.mhack.aggregate.server.api.user.domain.profile.Profile;
 
 public class User {
 
     private String _key;
     private String displayName;
     private String password;
-    private String name;
-    private ProfileLocation location;
-    private HashSet<FarmEquipment> farmEquipment;
-    private int farmSize;
-    private HashSet<FarmType> farmTypes;
+    private Profile profile;
 
-    public User(String _key, String displayName, String password, String name, ProfileLocation location,
-                HashSet<FarmEquipment> farmEquipment, int farmSize, HashSet<FarmType> farmTypes) {
+    public User(String _key, String displayName, String password, Profile profile) {
         this._key = _key;
         this.displayName = displayName;
         this.password = password;
-        this.farmEquipment = farmEquipment;
-        this.farmSize = farmSize;
-        this.farmTypes = farmTypes;
+        this.profile = profile;
     }
 
-    public String get_key() {
+    public String get_Key() {
         return _key;
     }
 
@@ -39,43 +28,11 @@ public class User {
         return password;
     }
 
-    public String getName() {
-        return name;
+    public Profile getProfile() {
+        return profile;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ProfileLocation getLocation() {
-        return location;
-    }
-
-    public void setLocation(ProfileLocation location) {
-        this.location = location;
-    }
-
-    public HashSet<FarmEquipment> getFarmEquipment() {
-        return farmEquipment;
-    }
-
-    public void setFarmEquipment(HashSet<FarmEquipment> farmEquipment) {
-        this.farmEquipment = farmEquipment;
-    }
-
-    public int getFarmSize() {
-        return farmSize;
-    }
-
-    public void setFarmSize(int farmSize) {
-        this.farmSize = farmSize;
-    }
-
-    public HashSet<FarmType> getFarmTypes() {
-        return farmTypes;
-    }
-
-    public void setFarmTypes(HashSet<FarmType> farmTypes) {
-        this.farmTypes = farmTypes;
+    public void setProfile(Profile profile) {
+        this.profile = profile;
     }
 }
